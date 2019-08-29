9bb92a193bd322bdd9da657fad775d23bc214699
chensheng <742611497@qq.com>
2019-08-29 10:43:51 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
 # tamboot-cloud-admin
+
+Tamboot Cloud Admin是一个基于 [tamboot cloud](https://github.com/chensheng/tamboot-cloud.git)（一个基于[spring cloud](https://spring.io/projects/spring-cloud)的微服务框架） 的微服务脚手架项目，实现了常见的用户管理、角色管理、权限管理、菜单管理等基本功能，开发者可基于此快速搭建微服务系统。
+
+* [模块介绍](#模块介绍)
+* [模块命名](#模块命名)
+* [快速上手](#快速上手)
+* [tamboot cloud教程](https://github.com/chensheng/tamboot-cloud/wiki)
+* [tamboot教程](https://github.com/chensheng/tamboot/wiki)
+* [前端项目](https://github.com/chensheng/tamboot-cloud-admin-frontend)
+
+
+## 模块介绍
+
+### tamboot-cloud-admin-eureka-server
+Eureka服务注册中心：集成了服务权限校验功能，当配置项`spring.security.user.name`和`spring.security.user.password`存在时，自动开启服务权限校验功能。如果服务权限校验功能开启，则服务消费端的eureka配置地址需添加权限信息，如：` http://user:password@localhost:8761/eureka/`。
+
+### tamboot-cloud-admin-security
+系统权限校验模块：在需要权限校验的应用中引入该模块，系统自动根据配置的权限信息来校验用户的访问权限。
+
+### tamboot-cloud-admin-system-ms
+系统微服务：提供用户、角色、菜单、权限等基础服务，在`tamboot-cloud-admin-security`、`tamboot-cloud-admin-system-app`模块中均有使用这些服务。
+
+### tamboot-cloud-admin-system-app
+系统应用：实现了用户管理、角色管理、菜单管理、权限管理等后台管理功能。
+
+
+## 模块命名
+原则上微服务模块命名以ms（microservice）结尾，应用模块命名以app结尾。
+
+
+## 快速上手
+* [安装依赖](#安装依赖)
+* [下载项目](#下载项目)
+* [创建数据库](#创建数据库)
+* [启动redis](#启动redis)
+* [运行服务注册中心](#运行服务注册中心)
+* [运行系统服务](#运行系统服务)
+* [运行系统应用](#运行系统应用)
+* [返回值](#返回值)
+* [状态码](#状态码)
+* [初始用户](#初始用户)
+
+### 安装依赖
+由于[tamboot](https://github.com/chensheng/tamboot.git)和[tamboot cloud](https://github.com/chensheng/tamboot-cloud.git)尚未发布到maven仓库，因此运行项目前，需要先在本地编译安装。
+
+```bash
+$ git clone https://github.com/chensheng/tamboot.git
+$ cd tamboot
+$ mvn install
+```
+
+```bash
+$ git clone https://github.com/chensheng/tamboot-cloud.git
+$ cd tamboot-cloud
+$ mvn install
+```
+
+### 下载项目
+```bash
+$ git clone https://github.com/chensheng/tamboot-cloud-admin.git
+```
+
+### 创建数据库
+数据库初始化文件在项目的`sql/`目录下。
+
+### 启动redis
+由于系统的权限信息存储在redis中，所以需要启动redis，具体的redis安装启动步骤可查看[redis官网](https://redis.io/)。
+
+### 运行服务注册中心
+运行完，可访问`http://localhost:8761/`验证。
+```bash
+cd tamboot-cloud-admin-eureka-server
+mvn spring-boot:run
+```
+
+### 运行系统服务
+运行完，可查看服务注册中心后台，验证服务是否注册成功。
+```bash
+cd tamboot-cloud-admin-system-ms
+mvn spring-boot:run
+```
+
+### 运行系统应用
+```bash
+cd tamboot-cloud-admin-system-app
+mvn spring-boot:run
+```
+
+### 返回值
+所有接口统一返回json格式数据，返回格式如下：
+```json
+{
+  "code": "1",
+  "msg": "success",
+  "data": null
+}
+```
+
+字段|说明
+-----|-----
+code|状态码。除了使用框架定义的状态码，开发者也可以自定义状态码。具体见下表。
+msg|提示信息
+data|数据。不同接口返回不同数据。
+
+### 状态码
+code|说明
+-----|-----
+0|失败
+1|成功
+1001|未登录
+1002|无权限
+9999|系统异常
+
+### 初始用户
+系统初始用户：admin，初始密码：Tamboot@123456
--- sql/tamboot_cloud_db.sql
+++ sql/tamboot_cloud_db.sql
@@ -23,9 +23,9 @@ CREATE TABLE `system_menu` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `path` varchar(64) DEFAULT NULL,
   `locale` varchar(128) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
@@ -41,10 +41,10 @@ CREATE TABLE `system_menu` (
 INSERT INTO `system_menu` VALUES ('6533916193823133696', '7', '2019-05-14 12:10:14', '1', '2019-05-16 11:23:41', '1', '/system', null, '系统配置', 'setting', null, '0');
 INSERT INTO `system_menu` VALUES ('6533916677858398211', '19', '2019-05-14 12:12:10', '1', '2019-06-02 01:04:51', '1', '/system/menu', null, '菜单管理', null, '6533916193823133696', '2');
 INSERT INTO `system_menu` VALUES ('6533916797031157764', '8', '2019-05-14 12:12:38', '1', '2019-06-02 01:04:56', '1', '/system/role', null, '角色管理', null, '6533916193823133696', '3');
-INSERT INTO `system_menu` VALUES ('6533917218659373061', '10', '2019-05-14 12:14:19', '1', '2019-06-02 22:18:26', '1', '/user', null, '用户中心', 'user', null, '2');
-INSERT INTO `system_menu` VALUES ('6533917345537069062', '3', '2019-05-14 12:14:49', '1', '2019-06-02 22:39:47', '1', '/user/password', null, '修改密码', null, '6533917218659373061', '0');
 INSERT INTO `system_menu` VALUES ('6536608913624666112', '2', '2019-05-21 22:30:09', '1', '2019-06-02 01:04:59', '1', '/system/permission', null, '访问权限', null, '6533916193823133696', '4');
 INSERT INTO `system_menu` VALUES ('6536612123282247681', '1', '2019-05-21 22:42:54', '1', '2019-06-02 01:04:39', '1', '/system/user', null, '用户管理', null, '6533916193823133696', '1');
+INSERT INTO `system_menu` VALUES ('6533917218659373061', '10', '2019-05-14 12:14:19', '1', '2019-06-02 22:18:26', '1', '/user', null, '用户中心', 'user', null, '2');
+INSERT INTO `system_menu` VALUES ('6533917345537069062', '3', '2019-05-14 12:14:49', '1', '2019-06-02 22:39:47', '1', '/user/password', null, '修改密码', null, '6533917218659373061', '0');
 
 -- ----------------------------
 -- Table structure for system_permission
@@ -54,9 +54,9 @@ CREATE TABLE `system_permission` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `url` varchar(64) DEFAULT NULL,
   `roles` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -78,9 +78,9 @@ CREATE TABLE `system_role` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `role_code` varchar(16) DEFAULT NULL,
   `role_name` varchar(32) DEFAULT NULL,
   `role_desc` varchar(128) DEFAULT NULL,
@@ -102,9 +102,9 @@ CREATE TABLE `system_role_menu` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `role_code` varchar(16) DEFAULT NULL,
   `menu_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -118,11 +118,8 @@ INSERT INTO `system_role_menu` VALUES ('6533981267984125954', '0', null, null, n
 INSERT INTO `system_role_menu` VALUES ('6533981268063817732', '0', null, null, null, null, 'MANAGER', '6533916797031157764');
 INSERT INTO `system_role_menu` VALUES ('6533981268088983557', '0', null, null, null, null, 'MANAGER', '6533917345537069062');
 INSERT INTO `system_role_menu` VALUES ('6533981268114149382', '0', null, null, null, null, 'MANAGER', '6533917218659373061');
-INSERT INTO `system_role_menu` VALUES ('6536481623137849344', '0', null, null, null, null, 'USER', '6533917345537069062');
-INSERT INTO `system_role_menu` VALUES ('6536481623225929729', '0', null, null, null, null, 'USER', '6533917218659373061');
 INSERT INTO `system_role_menu` VALUES ('6536609193594458112', '0', null, null, null, null, 'MANAGER', '6536608913624666112');
 INSERT INTO `system_role_menu` VALUES ('6540485233114877952', '0', null, null, null, null, 'MANAGER', '6536612123282247681');
-INSERT INTO `system_role_menu` VALUES ('6541132633567399936', '0', null, null, null, null, 'MANAGER', '6541132351248797696');
 
 -- ----------------------------
 -- Table structure for system_user
@@ -132,9 +129,9 @@ CREATE TABLE `system_user` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `status` tinyint(2) DEFAULT NULL,
@@ -145,7 +142,7 @@ CREATE TABLE `system_user` (
 -- ----------------------------
 -- Records of system_user
 -- ----------------------------
-INSERT INTO `system_user` VALUES ('1', '0', null, null, null, null, 'admin', '{bcrypt}$2a$10$XlxuthlC.aiqv4FQ0vk8N.2ECtkg4.4dKrAsRN9upqDLY5Or.YZja', '1');
+INSERT INTO `system_user` VALUES ('1', '0', null, null, null, null, 'admin', '{bcrypt}$2a$10$wJ78miQmI0k7PL70bUkjIOqmvKm1tkEsnN2JWP6bjB.wNl6Hm61iq', '1');
 INSERT INTO `system_user` VALUES ('2', '0', null, null, null, null, 'user', '{bcrypt}$2a$10$3lRGVEr06pErbsUxDqh1JeNocMA.pn/9SrRbS4WfyrZgXqRC87tyO', '1');
 
 -- ----------------------------
@@ -156,9 +153,9 @@ CREATE TABLE `system_user_role` (
   `id` bigint(20) NOT NULL,
   `version` bigint(20) DEFAULT '0',
   `create_time` datetime DEFAULT NULL,
-  `creator` int(11) DEFAULT NULL,
+  `creator` bigint(20) DEFAULT NULL,
   `modify_time` datetime DEFAULT NULL,
-  `modifier` int(11) DEFAULT NULL,
+  `modifier` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `role_code` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id`),
